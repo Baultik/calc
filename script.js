b6887d0ca3ec326e002da115d15445c3a1689268
Brian Ault <baultik@gmail.com>
2016-12-06 12:41:32 -0800

    fixed bug in find highest - added comments

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 /**
  * Created by baultik on 11/22/16.
  */
-
-//var my_calculator = new calculator(callback);
-
 var calc = new Calculator();
 var display = null;
 var typeEnum = {
@@ -15,69 +12,125 @@ var typeEnum = {
 
 $(document).ready(function () {
     $(".btn").on("click", handleClick);
+    $(document).on("keypress", handleKeyPress);
+    $(document).on("keydown", handleSpecialKeys);
     display = new Display();
 
 });
-
-//
-
+/**
+ * Button click handler
+ * @param event The click event
+ */
 function handleClick(event) {
-    var val = $(this).text();
+    var button = $(this);
+    button.blur(); //remove focus
+    var val = button.text();
     calc.addInput(val);
 }
+/**
+ * Special key handler. For the delete key to clear the caclulator
+ * @param event The keydown event
+ */
+function handleSpecialKeys(event) {
+    if (event.which == 46) {
+        //delete key - doesn't trigger keypress
+        //console.log("Delete " + event.charCode + " " + event.which);
+        calc.addInput("C");//C means clear
+    }
+}
+/**
+ * Main key handler. Matches the numbers, operators, and enter/=.
+ * Key press gives the true character code from event.which that I match to the button text elements
+ * @param event The keypress handler
+ */
+function handleKeyPress(event) {
+    var keyAsChar = String.fromCharCode(event.which);
+    var pattern = /[0-9./*\-+\r=]/;
 
+    if (keyAsChar.match(pattern) !== null) {
+        console.log("Found Match " + keyAsChar + " " + event.which);
+        if (keyAsChar === "*") {
+            keyAsChar = "×";
+        } else if (keyAsChar === "/") {
+            keyAsChar = "÷";
+        } else if (event.which === 13) {
+            keyAsChar = "=";
+        }
+        calc.addInput(keyAsChar);
+    } else {
+        //console.log("No Match " + event.which + " " + keyAsChar);
+    }
+}
+/**
+ * Calculator which handles the calculations and receives the input.
+ * @constructor
+ */
 function Calculator() {
-    var entryQueue = [];
-    var operationStart = 1;
-    var total = 0;
+    var mEntryQueue = [];
+    var mOperationStart = 1;
+    var mTotal = 0;
 
+    /**
+     * Accepts and handles user input
+     * @param input The user input {@link Entry} object
+     */
     this.addInput = function (input) {
-        var lastIndex = entryQueue.length - 1;
-        var lastEntry = entryQueue[lastIndex];
+        //Check if can append or replace input
+        var lastIndex = mEntryQueue.length - 1;
+        var lastEntry = mEntryQueue[lastIndex];
         if (lastEntry && lastEntry.add(input)) {
-            //entry exists and is number - add to it
-            //update with current value - number ie 1.2
+            //entry exists and is number -> add to it - or is operator -> replace it
+            //update display with current value - ie 1.2
             if (lastEntry.type() === typeEnum.number ) display.updateDisplay(lastEntry.value());
             setInitial(lastEntry);
-            display.updateCalculation(entryQueue);
+            display.updateCalculation(mEntryQueue);
             return;
         }
 
+        //new entry
         var entry = new Entry(input);
-        entryQueue.push(entry);
+        mEntryQueue.push(entry);
 
-        display.updateCalculation(entryQueue);
+        display.updateCalculation(mEntryQueue);
 
         if (entry.type() === typeEnum.operator || entry.type() === typeEnum.equalSign) {
-
-            //total = calculate(total,operationStart,entryQueue);
-            total = orderOfOpsAttempt(entryQueue);
+            //mTotal = calculate(mTotal,mOperationStart,mEntryQueue);
+            mTotal = orderOfOps(mEntryQueue);
 
             if (entry.type() === typeEnum.operator) {
-                //update operation index
-                operationStart = lastIndex + 1;
+                mOperationStart = lastIndex + 1;
             } else {
-
                 display.clearCalculation();
             }
         } else if (entry.type() === typeEnum.number) {
             setInitial(entry);
+            //mTotal = orderOfOps(mEntryQueue);
             display.updateDisplay(input);
         } else if (entry.type() === typeEnum.clear) {
-            total = 0;
-            entryQueue = [];
-            display.updateDisplay(total);
+            mTotal = 0;
+            mEntryQueue = [];
+            display.updateDisplay(mTotal);
             display.clearCalculation();
         }
     };
-
+    /**
+     * If the first entry is a number, display that number
+     * @param entry The {@link Entry} object
+     */
     function setInitial(entry) {
-        if (entry.type() === typeEnum.number && entryQueue.length == 1) {
+        if (entry.type() === typeEnum.number && mEntryQueue.length == 1) {
             //first item
-            total = parseFloat(entry.value());
+            mTotal = parseFloat(entry.value());
         }
     }
 
+    /**
+     * Calculates an operation
+     * @param currentTotal The total to add to or the left operand
+     * @param operatorIndex The index of the operator in the queue
+     * @param queue The queue with the operator and right operand
+     * @returns {*} The number calculated
+     */
     function calculate(currentTotal,operatorIndex,queue) {
         var operatorEntry = queue[operatorIndex];
         var numberEntry = queue[operatorIndex+1];
@@ -116,6 +169,11 @@ function Calculator() {
         return currentTotal;
     }
 
+    /**
+     * Total the numbers of '=' in the queue. More than one
+     * @param queue The current queue of {@link Entry} objects
+     * @returns {number}
+     */
     function equalsCount(queue){
         var equals = 0;
         for (var i = 0; i < queue.length; i++) {
@@ -127,30 +185,38 @@ function Calculator() {
         return equals;
     }
 
-    function orderOfOpsAttempt(entryQueue) {
-        var operations = entryQueue.slice();
+    /**
+     * Order of operators calculation. Finds the operators and performs the operations
+     * @param queue The queue of {@link Entry} objects
+     * @returns {number} The total calculated
+     */
+    function orderOfOps(queue) {
+        var operations = queue.slice();
         var total = 0;
         var operatorEntry = null;
         var numberEntry = null;
 
+        //Use copy of queue - handle operations by precedence and order - splice answers into array
         while (operations.length > 0) {
-            var highest = findHighest(operations);
+            var highest = findHighestOperator(operations);
             if (highest === null) {
+                //Handle extra = in multiple = entry
                 var equal = operations[operations.length - 1];
                 if (equal && equal.type() === typeEnum.equalSign && equalsCount(operations) > 1) {
                     operations.splice(1,1,operatorEntry,numberEntry);
-                    highest = findHighest(operations);
+                    highest = findHighestOperator(operations);
                     if (highest === null) break;
                 } else {
                     break;
                 }
             }
 
+            //Need an operator and number to perform operation
             var left = operations[highest.index-1];
             operatorEntry = operations[highest.index];
             numberEntry = operations[highest.index+1];
-            if (!operatorEntry || !numberEntry) break;
 
+            //If an initial/left value exists - use it and splice it out - else use operator and number
             var current = 0;
             var startSplice = highest.index;
             var spliceCount = 2;
@@ -161,29 +227,36 @@ function Calculator() {
             }
 
             total = calculate(parseFloat(current),highest.index,operations);
-
-            operations.splice(startSplice,spliceCount,new Entry(total));
+            //if (!operatorEntry || !numberEntry) break;
+            if (operatorEntry && numberEntry) {
+                operations.splice(startSplice, spliceCount, new Entry(total));
+            }
         }
-
+        //console.log("calculation done");
+        //display.updateDisplay(total);
         return total;
     }
 
-    this.tryIt = function() {
-        var queue = [new Entry(1),new Entry("+"),new Entry(3),new Entry("÷"),new Entry(4),new Entry("+"),new Entry(10),new Entry("×"),new Entry(2)];
-        return orderOfOpsAttempt(queue);
-    };
-
-    function findHighest(entryQueue) {
+    /**
+     * Finds the highest operator based on precedence and position
+     * @param queue The current queue {@link Entry} objects
+     * @returns {*}
+     */
+    function findHighestOperator(queue) {
         var highest = null;
         //get order of operations
-        for (var i = 0; i < entryQueue.length; i++) {
-            var entry = entryQueue[i];
+        for (var i = 0; i < queue.length; i++) {
+            var entry = queue[i];
             var precedence = 1;
             if (entry && entry.type() === typeEnum.operator) {
+                var numberEntry = queue[i+1];
+                if (!numberEntry) continue;
+
+                //Only find operators - times and divide have higher precedence - use number not a possible index
                 if (entry.value() ===  "÷" || entry.value() ===  "×") {
-                    precedence = entryQueue.length * 10;
+                    precedence = queue.length * 10;
                 }
-                var val = (entryQueue.length - i ) * precedence;
+                var val = (queue.length - i ) * precedence;
                 if (highest === null)highest = {index:0,value:0};
                 if (val > highest.value) {
                     highest.index = i;
@@ -196,15 +269,26 @@ function Calculator() {
         return highest;
     }
 }
-
+/**
+ * Represents a user input as an object
+ * @param value The input from the user
+ * @constructor
+ */
 function Entry(value) {
     var mValue = value;
     var mType = null;
 
+    /**
+     * Value accessor function
+     * @returns {*} The value
+     */
     this.value = function () {
         return mValue;
     };
-
+    /**
+     * Type accessor function
+     * @returns {*} The type
+     */
     this.type = function () {
         if (mType === null) {
             mType = getType(mValue);
@@ -212,7 +296,11 @@ function Entry(value) {
 
         return mType;
     };
-
+    /**
+     * Attempt to add user inputs. Try to concatenate numbers and replace operators. Ignore other types
+     * @param value The user input
+     * @returns {boolean} Whether or not the input was handled
+     */
     this.add = function (value) {
         if (getType(value) !== this.type()) return false;
 
@@ -222,7 +310,7 @@ function Entry(value) {
                     mValue = mValue.slice(0, mValue.length - 1);
                 }
                 mValue += value;
-                display.updateDisplay(mValue);
+                //display.updateDisplay(mValue);
                 return true;
             case typeEnum.operator:
                 mValue = value;
@@ -230,7 +318,11 @@ function Entry(value) {
         }
         return false;
     };
-
+    /**
+     * Get a normalized type from the value
+     * @param value The value
+     * @returns {string} The normalized type
+     */
     var getType = function (value) {
         var type = "";
 
@@ -255,23 +347,34 @@ function Entry(value) {
         return type;
     }
 }
-
+/**
+ * Display a value in the DOM
+ * @constructor
+ */
 function Display() {
     var display = $("#display");
     var calculation = $("#calculation");
-
+    /**
+     * Update the main display with a value
+     * @param toDisplay The value to display
+     */
     this.updateDisplay = function (toDisplay) {
         display.html(toDisplay);
     };
-
-    this.updateCalculation = function (arrayToDisplay) {
+    /**
+     * Update the calculation read out from the queue
+     * @param queueToDisplay The queue of {@link Entry} objects
+     */
+    this.updateCalculation = function (queueToDisplay) {
         var toDisplay = "";
-        for (var i in arrayToDisplay) {
-            toDisplay += arrayToDisplay[i].value();
+        for (var i in queueToDisplay) {
+            toDisplay += queueToDisplay[i].value();
         }
         calculation.html(toDisplay);
     };
-
+    /**
+     * Clear the calculation read out
+     */
     this.clearCalculation = function () {
         calculation.html(" ");
     };
